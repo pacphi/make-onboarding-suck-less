c1e324437c3e9302e80e877710ccdc52d72d01ca
Chris Phillipson <cphillipson@pivotal.io>
2021-08-30 14:36:37 -0700

    Small tweaks to fetch script
    * to support prefetch check of Windows or *Nix CLI

--- scripts/fetch-tanzu-cli.sh
+++ scripts/fetch-tanzu-cli.sh
@@ -12,9 +12,16 @@ TANZU_VERSION="$4"
 
 FILE=tanzu-cli-bundle-v${TANZU_VERSION}-${OS}-amd64.tar
 CURRENT_VERSION="latest"
-if [ -e "dist/tanzu" ]; then
+if [ "$OS" == "windows" ]; then
+  TANZU_CLI = "tanzu.exe"
+  DIST_EXECUTABLE = "dist/tanzu.exe"
+else
+  TANZU_CLI="tanzu"
+  DIST_EXECUTABLE = "dist/tanzu"
+fi
+if [ -e "$DIST_EXECUTABLE" ]; then
   cd dist
-  TANZU_CLI_VERSION_OUTPUT=$(tanzu version)
+  TANZU_CLI_VERSION_OUTPUT=$(${TANZU_CLI} version)
   MULTI_LINE_STRING=${TANZU_CLI_VERSION_OUTPUT#"version: v"}
   OUTPUT_ARRAY=(${MULTI_LINE_STRING[@]})
   CURRENT_VERSION=${OUTPUT_ARRAY[0]}
@@ -31,10 +38,10 @@ else
     vmw-cli cp tanzu-cli-bundle-v${TANZU_VERSION}-${OS}-amd64.tar
     tar xvf tanzu-cli-bundle-v${TANZU_VERSION}-${OS}-amd64.tar -C dist
     if [ "$OS" == "windows" ]; then
-      cp dist/cli/core/v${TANZU_VERSION}/tanzu-core-${OS}_amd64.exe dist/tanzu.exe
+      cp dist/cli/core/v${TANZU_VERSION}/tanzu-core-${OS}_amd64.exe ${DIST_EXECUTABLE}
     else
       chmod +x dist/cli/core/v${TANZU_VERSION}/tanzu-core-${OS}_amd64
-      cp dist/cli/core/v${TANZU_VERSION}/tanzu-core-${OS}_amd64 dist/tanzu
+      cp dist/cli/core/v${TANZU_VERSION}/tanzu-core-${OS}_amd64 ${DIST_EXECUTABLE}
     fi
     rm -Rf dist/cli
     rm -f tanzu-cli-bundle-v${TANZU_VERSION}-${OS}-amd64.tar
