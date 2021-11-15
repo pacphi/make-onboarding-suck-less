8c14c9ad1988cc9e96c46386a7f2b5e7baf82076
Chris Phillipson <cphillipson@pivotal.io>
2021-11-14 21:59:37 -0800

    Create ca-key-pair secret from template rather than imperatively
    * So we can include CA

--- scripts/tanzu/tap/uninstall-mkcert-for-tkg-on-aws.sh
+++ scripts/tanzu/tap/uninstall-mkcert-for-tkg-on-aws.sh
@@ -4,13 +4,13 @@ set -e
 # Remove mkcert managed Certificate plus Secret and ClusterIssuer
 
 ## Delete ClusterIssuer
-kubectl delete clusterissuer mkcert -n cert-manager
+kubectl delete clusterissuer ca-issuer -n cert-manager
 
 ## Delete Certificate
 kubectl delete cert knative-tls -n contour-external
 
 ## Delete Secrets
-kubectl delete secret mkcert-secret -n cert-manager
+kubectl delete secret ca-key-pair -n cert-manager
 kubectl delete secret knative-tls -n contour-external
 kubectl delete secret knative-tls -n educates
 
