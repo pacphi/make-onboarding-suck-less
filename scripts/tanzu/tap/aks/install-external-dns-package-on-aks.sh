73772cb6379f1338063223baed103234054a9565
Chris Phillipson <cphillipson@pivotal.io>
2021-12-12 07:02:03 -0800

    WIP - Change to external DNS config for Azure
    - still not complete

--- scripts/tanzu/tap/aks/install-external-dns-package-on-aks.sh
+++ scripts/tanzu/tap/aks/install-external-dns-package-on-aks.sh
@@ -24,29 +24,26 @@ namespace: tanzu-system-service-discovery
 
 # Deployment-related configuration.
 deployment:
-  args:
-  - --source=service
-  - --source=ingress
-  - --source=contour-httpproxy # Provide this to enable Contour HTTPProxy support. Must have Contour installed or ExternalDNS will fail.
-  - --domain-filter=${DOMAIN} # Makes ExternalDNS see only the hosted zones matching provided domain, omit to process all available hosted zones.
-  - --policy=upsert-only # Prevents ExternalDNS from deleting any records, omit to enable full synchronization.
-  - --registry=txt
-  - --txt-owner-id=external-dns
-  - --txt-prefix=txt # Disambiguates TXT records from CNAME records.
-  - --provider=google
-  - --google-project=${PROJECT_ID}
-  env:
-  - name: GOOGLE_APPLICATION_CREDENTIALS
-    value: /secrets/external-dns-admin-credentials/credentials.json
-  securityContext: {}
-  volumeMounts:
-  - mountPath: /secrets/external-dns-admin-credentials
-    name: external-dns-admin-credentials
-    readOnly: true
-  volumes:
-  - name: external-dns-admin-credentials
-    secret:
-      secretName: external-dns-admin-credentials
+ args:
+   - --source=service
+   - --source=ingress
+   - --source=contour-httpproxy # Provide this to enable Contour HTTPProxy support. Must have Contour installed or ExternalDNS will fail.
+   - --domain-filter=DOMAIN # For example, k8s.example.org. Makes ExternalDNS see only the hosted zones matching provided domain, omit to process all available hosted zones.
+   - --policy=upsert-only # Prevents ExternalDNS from deleting any records, omit to enable full synchronization.
+   - --registry=txt
+   - --txt-prefix=externaldns- # Disambiguates TXT records from CNAME records.
+   - --provider=azure
+   - --azure-resource-group=RESOURCE-GROUP # Azure resource group.
+ env: []
+ securityContext: {}
+ volumeMounts:
+   - name: azure-config-file
+     mountPath: /etc/kubernetes
+     readOnly: true
+ volumes:
+   - name: azure-config-file
+     secret:
+       secretName: azure-config-file
 EOF
 
 tanzu package install external-dns -p external-dns.tanzu.vmware.com -v 0.8.0+vmware.1-tkg.1 --values-file external-dns-data-values.yaml -n tanzu-package-repo-global
