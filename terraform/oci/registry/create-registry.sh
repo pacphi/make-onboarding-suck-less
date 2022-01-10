2b1d9881de8f9e7ddd2b94b435e80330ff959b0d
Chris Phillipson <cphillipson@pivotal.io>
2022-01-09 22:25:28 -0800

    Container image repository Terraform is wonky; fallback to oci client

--- terraform/oci/registry/create-registry.sh
+++ terraform/oci/registry/create-registry.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-terraform init
-terraform validate
-terraform graph | dot -Tsvg > graph.svg
-terraform plan -out terraform.plan
-terraform apply -auto-approve -state terraform.tfstate terraform.plan
+#
+#terraform init
+#terraform validate
+#terraform graph | dot -Tsvg > graph.svg
+#terraform plan -out terraform.plan
+#terraform apply -auto-approve -state terraform.tfstate terraform.plan
+
+if [ -z "$1" ] && [ -z "$2" ]; then
+  echo "Usage: create-registry.sh {compartment-ocid} {repository-name}"
+  exit 1
+fi
+
+COMPARTMENT_OCID="$1"
+REPOSITORY_NAME="$2"
+
+oci artifacts container repository create --compartment-id ${COMPARTMENT_OCID} --display-name ${REPOSITORY_NAME}
 
 echo "Please consult https://docs.oracle.com/en-us/iaas/Content/Registry/Tasks/registrypushingimagesusingthedockercli.htm in order to create and use an Auth Token with this repository."
--- terraform/oci/registry/main.tf
+++ terraform/oci/registry/main.tf
@@ -5,6 +5,16 @@ resource "random_string" "suffix" {
   special          = false
 }
 
+resource "oci_identity_auth_token" "auth_token" {
+  description = "Auth token for container image repository"
+  user_id = var.user_ocid
+}
+
+resource "oci_artifacts_container_configuration" "cr_config" {
+  compartment_id = var.compartment_ocid
+  is_repository_created_on_first_push = false
+}
+
 resource "oci_artifacts_container_repository" "cr" {
   compartment_id = var.compartment_ocid
   display_name = "${var.display_name}-${random_string.suffix.result}"
